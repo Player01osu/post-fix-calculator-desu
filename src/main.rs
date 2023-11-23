33f34c7f80de3336eaccccb4ae68ff7ff290b66c
player01osu <player01osu@gmail.com>
2023-11-23 17:04:31 -0500

    Refactor to be postfix and operators act on two values on stack

--- src/main.rs
+++ src/main.rs
@@ -1,63 +1,77 @@
 use std::{collections::VecDeque, str::FromStr, io::{stdin, stdout}};
+use std::io::Write;
 
-#[derive(Debug)]
+#[derive(Debug, Clone, Copy)]
 pub enum Operator {
-    Plus,
-    Multiply,
+    Add,
+    Sub,
+    Mult,
+    Div,
 }
 
 #[derive(Debug)]
 pub enum CalcToken {
-    Literal(i32),
+    Literal(f64),
     Operator(Operator),
-    // End of expression
-    EOE
+    PrintStack,
+    ClearStack,
 }
+
+type Stack = VecDeque<f64>;
 
 impl FromStr for CalcToken {
     type Err = String;
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         match s.trim() {
-            "+" => Ok(CalcToken::Operator(Operator::Plus)),
-            "*" => Ok(CalcToken::Operator(Operator::Multiply)),
-            "=" => Ok(CalcToken::EOE),
-            s => Ok(CalcToken::Literal(s.parse::<i32>().map_err(|e| e.to_string())?)),
+            "+" => Ok(CalcToken::Operator(Operator::Add)),
+            "-" => Ok(CalcToken::Operator(Operator::Sub)),
+            "*" => Ok(CalcToken::Operator(Operator::Mult)),
+            "/" => Ok(CalcToken::Operator(Operator::Div)),
+            "p" => Ok(CalcToken::PrintStack),
+            "c" => Ok(CalcToken::ClearStack),
+            s => Ok(CalcToken::Literal(s.parse::<f64>().map_err(|e| e.to_string())?)),
         }
     }
 }
 
-type CalcStack = VecDeque<CalcToken>;
+fn print_stack(stack: &Stack) {
+    for i in stack {
+        println!("{i}");
+    }
+}
 
-pub fn process_operator(stack: &mut CalcStack, operator: Operator) {
-    let mut acc = None;
-    while let Some(CalcToken::Literal(num)) = stack.pop_front() {
-        match acc.as_mut() {
-            Some(acc) => {
-                match operator {
-                    Operator::Plus => *acc += num,
-                    Operator::Multiply => *acc *= num,
-                }
-            }
-            None => acc = Some(num),
+fn pop_two(stack: &mut Stack) -> Option<(f64, f64)> {
+    if let Some(a) = stack.pop_front() {
+        if let Some(b) = stack.pop_front() {
+            return Some((a, b));
         }
+        stack.push_front(a);
+    }
+    None
+}
+
+fn pop_two_warn(stack: &mut Stack) -> Option<(f64, f64)> {
+    pop_two(stack).or_else(|| {
+        eprintln!("Stack is empty");
+        None
+    })
+}
+
+fn exec_operator(stack: &mut Stack, operator: Operator) {
+    let (a, b) = match pop_two_warn(stack) {
+        Some(v) => v,
+        None => return,
     };
-    let acc = acc.unwrap();
-    println!("{acc}");
-}
-
-pub fn process_stack(stack: &mut CalcStack) {
-    while let Some(token) = stack.pop_front() {
-        match token {
-            CalcToken::Operator(operator) => process_operator(stack, operator),
-            k => panic!("Expected an operator, got {k:?}"),
-        }
+    match operator {
+        Operator::Add => stack.push_front(a + b),
+        Operator::Sub => stack.push_front(a - b),
+        Operator::Mult => stack.push_front(a * b),
+        Operator::Div => stack.push_front(a / b),
     }
 }
-
-use std::io::Write;
 
 fn main() {
-    let mut queue = VecDeque::<CalcToken>::new();
+    let mut stack = Stack::new();
     let mut stdout = stdout();
 
     print!("calc-desu> ");
@@ -66,10 +80,10 @@ fn main() {
         match lines.parse() {
             Ok(token) => {
                 match token {
-                    CalcToken::EOE => {
-                        process_stack(&mut queue);
-                    }
-                    _ => queue.push_back(token)
+                    CalcToken::PrintStack => print_stack(&stack),
+                    CalcToken::ClearStack => stack.clear(),
+                    CalcToken::Operator(operator) => exec_operator(&mut stack, operator),
+                    CalcToken::Literal(n) => stack.push_back(n),
                 }
             }
             Err(e) => println!("{e}"),
@@ -78,6 +92,4 @@ fn main() {
         print!("calc-desu> ");
         stdout.flush().unwrap();
     }
-
-    process_stack(&mut queue);
 }
